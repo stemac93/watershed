7180c5de89535d1626fccc3f929658611d728a13
stemac93 <38649676+stemac93@users.noreply.github.com>
2023-01-18 19:30:12 +0100

    Create zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,24 @@
+.onLoad = function(libname, pkgname) {
+	Sys.setenv("GRASS_VERBOSE"=0)
+	rgrass::use_sp()
+
+	if(is.null(getOption("mc.cores")) && !grepl("[Ww]indows", Sys.info()['sysname'])) {
+		message("For faster performance of some functions on a machine with lots of RAM, you can set")
+		message("options(mc.cores = parallel::detectCores())")
+	}
+
+	## set of an environment to hold package globals
+	assign("ws_env", new.env(), envir = parent.env(environment()))
+	ws_env$rasters = list()
+	ws_env$vectors = list()
+
+	## Try to figure out where GRASS is located, warn the user if not found
+	gisBase = find_grass()
+	if(is.null(gisBase) | is.na(gisBase)) {
+		warning("Could not automatically detect GRASS, see ?find_grass")
+	} else {
+		message("Using grass: ", gisBase)
+		message("see ?find_grass to override this default")
+		options(gisBase = gisBase)
+	}
+}

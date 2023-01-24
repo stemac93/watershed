2ab5ef1cddf4c0bac29b68c8c25eb63f1d889d48
stemac93 <38649676+stemac93@users.noreply.github.com>
2023-01-24 11:30:47 +0100

    Update grass.r

--- R/grass.r
+++ R/grass.r
@@ -58,7 +58,7 @@
 #' @param x A single vector layer name to read
 #' @keywords internal
 .read_rasters = function(layers, file) {
-	suppressWarnings(val <- rgrass::readRAST(l))
+	suppressWarnings(val <- rgrass::read_RAST(l))
 	ras = sapply(layers, function(l) {
 		raster::raster(val)})
 	if(length(layers) > 1) {
